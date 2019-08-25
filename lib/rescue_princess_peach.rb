363d07cfbb21dfbf7f5b136c479432e6500de7df
Eric Johnson <ericjohnson0216@icloud.com>
2019-08-25 23:47:13 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
-def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
-end
+# def rescue_princess_peach
+#   status = "rescued"
+#   puts "Hooray! Mario has rescued Princess Peach."
+# end
 
 
 
@@ -10,7 +10,3 @@ end
 
 # rescue_princess_peach
 # puts status
-
-
-
-
